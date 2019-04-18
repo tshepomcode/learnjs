3bed6a8675a00eeb5394fac627eca089f8602cea
tshepomcode <tshepomcode@gmail.com>
2019-04-18 16:06:23 +0200

    Variables lessons completed

---
+++ app.js
@@ -0,0 +1,16 @@
+let name = "John";
+
+// embed a variable
+alert( `Hello, ${name}!` ); // Hello, John!
+
+// embed an expression
+console.log( `the result is ${1 + 2}` ); // the result is 3
+
+// typeof x;
+// typeof(x);
+
+typeof alert;
+console.log(typeof(alert));
+
+typeof Math;
+console.log("typeof Math or typeof(Math) // " + typeof(Math));
